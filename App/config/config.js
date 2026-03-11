e7db2f16f039b7f51e02733e5492b0932e897a48
Sandun441 <117435121+Sandun441@users.noreply.github.com>
2026-03-11 19:33:12 +0530

    build apk

--- App/config/config.js
+++ App/config/config.js
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 export const API_URL = "https://edutrack-pvh3.onrender.com/api";
+=======
+export const API_URL = "https://edutrack-pvh3.onrender.com";
+>>>>>>> Stashed changes
